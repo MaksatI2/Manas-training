be3bf37ba94b865dffbbf3294c6f627e632a72e3
MaksatI2 <zer0icemax@gmail.com>
2025-06-06 12:15:52 +0000

    Ms 006

---
+++ src/test/java/manasTrainingService/ManasTrainingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package manasTrainingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManasTrainingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
