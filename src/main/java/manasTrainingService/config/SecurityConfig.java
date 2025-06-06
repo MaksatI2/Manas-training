9b2b5e2c202d4449ffeebc82be584840806a7e89
Erjan  Tashmatov <tasmatoverzan05@gmail.com>
2025-06-06 21:13:51 +0600

    Ms 007

---
+++ src/main/java/manasTrainingService/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package manasTrainingService.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement( session ->
+                        session.sessionCreationPolicy( SessionCreationPolicy.ALWAYS ))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/auth/login?error=true")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .permitAll()
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/courses/**", "/", "/auth/**", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
