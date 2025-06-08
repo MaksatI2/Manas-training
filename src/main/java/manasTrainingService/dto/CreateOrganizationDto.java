2a2011e2780fcea92706340a0a3265b5aeadd38d
Erjan <tasmatoverzan05@gmail.com>
2025-06-08 20:28:44 +0600

    Изменены анотации

--- src/main/java/manasTrainingService/dto/CreateOrganizationDto.java
+++ src/main/java/manasTrainingService/dto/CreateOrganizationDto.java
@@ -1,12 +1,10 @@
 package manasTrainingService.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import manasTrainingService.entity.User;
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
