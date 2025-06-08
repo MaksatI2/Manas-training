4dbc41b652dc6abfa65d18ec1fc52fdcf528e056
Erjan <tasmatoverzan05@gmail.com>
2025-06-08 18:05:54 +0600

    Метод для получения айди роли организации (будет использоваться для регистрации организации)

---
+++ src/main/java/manasTrainingService/service/RoleService.java
@@ -0,0 +1,7 @@
+package manasTrainingService.service;
+
+import manasTrainingService.entity.Role;
+
+public interface RoleService {
+    Role getCompanyTypeId();
+}
---
+++ src/main/java/manasTrainingService/service/impl/RoleServiceImpl.java
@@ -0,0 +1,22 @@
+package manasTrainingService.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import manasTrainingService.entity.Role;
+import manasTrainingService.exceptions.RoleNotFoundException;
+import manasTrainingService.repositories.RoleRepository;
+import manasTrainingService.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public Role getCompanyTypeId(){
+        return roleRepository.findByName("ORGANIZATION")
+                .orElseThrow(()-> new RoleNotFoundException("Тип роли не найдена"));
+    }
+
+}
