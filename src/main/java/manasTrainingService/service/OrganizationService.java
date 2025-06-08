919acd37177aebd1104819d377e1a835cc20cbd0
Erjan <tasmatoverzan05@gmail.com>
2025-06-08 18:06:44 +0600

    Реализованы методы для создания профиля организации и генерации их уникального кода

---
+++ src/main/java/manasTrainingService/service/OrganizationService.java
@@ -0,0 +1,7 @@
+package manasTrainingService.service;
+
+import manasTrainingService.dto.CreateOrganizationDto;
+
+public interface OrganizationService {
+    void createOrganization(CreateOrganizationDto dto);
+}
---
+++ src/main/java/manasTrainingService/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,36 @@
+package manasTrainingService.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import manasTrainingService.dto.CreateOrganizationDto;
+import manasTrainingService.entity.Organization;
+import manasTrainingService.repositories.OrganizationRepository;
+import manasTrainingService.service.OrganizationService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrganizationServiceImpl implements OrganizationService {
+
+    private final OrganizationRepository organizationRepository;
+
+    @Override
+    public void createOrganization(CreateOrganizationDto dto){
+        Organization organization = Organization.builder()
+                .user(dto.getUser())
+                .code(generateOrganizationCode())
+                .build();
+        organizationRepository.save(organization);
+    }
+
+    private String generateOrganizationCode() {
+        int maxAttempts = 10000;
+        for (int i = 1; i <= maxAttempts; i++) {
+            String code = String.format("ORG%04d", i);
+            if (!organizationRepository.findByCode(code).isPresent()) {
+                return code;
+            }
+        }
+        throw new IllegalStateException("Не удалось сгенерировать уникальный код организации");
+    }
+
+}
