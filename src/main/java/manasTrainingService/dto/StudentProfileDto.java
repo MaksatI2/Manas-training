db4bc77b4b58a03c77da8c5727deaa0e246275cd
Erjan <tasmatoverzan05@gmail.com>
2025-06-08 20:29:23 +0600

    Созданы дто для регистрации студента и для создания профиля студента

---
+++ src/main/java/manasTrainingService/dto/StudentProfileDto.java
@@ -0,0 +1,15 @@
+package manasTrainingService.dto;
+
+import lombok.*;
+import manasTrainingService.entity.Organization;
+import manasTrainingService.entity.User;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentProfileDto {
+    private User student;
+    private Organization organization;
+}
