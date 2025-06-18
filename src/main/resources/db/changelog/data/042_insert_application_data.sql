a91234782849f3d29a0bec8f10fcaabde3c15d3c
Denchhik <denistronin30@gmail.com>
2025-06-18 23:55:43 +0600

    Bug fixed with Data base

--- src/main/resources/db/changelog/data/042_insert_application_data.sql
+++ src/main/resources/db/changelog/data/042_insert_application_data.sql
@@ -29,37 +29,34 @@ SELECT
       AND submitted_by = (SELECT id FROM users WHERE email = 'org1@manas.kg')
 );
 
-INSERT INTO course_application_employees (application_id, employee_id, application_status)
+INSERT INTO course_application_employees (application_id, employee_id)
 SELECT
     (SELECT id FROM course_applications
      WHERE course_id = (SELECT id FROM courses WHERE title = 'Курс 1')
        AND submitted_by = (SELECT id FROM users WHERE email = 'org1@manas.kg')),
-    (SELECT id FROM users WHERE email = 'student11@manas.kg'),
-    'PENDING'
+    (SELECT id FROM users WHERE email = 'student11@manas.kg')
     WHERE NOT EXISTS (
     SELECT 1 FROM course_application_employees
     WHERE employee_id = (SELECT id FROM users WHERE email = 'student11@manas.kg')
 );
 
-INSERT INTO course_application_employees (application_id, employee_id, application_status)
+INSERT INTO course_application_employees (application_id, employee_id)
 SELECT
     (SELECT id FROM course_applications
      WHERE course_id = (SELECT id FROM courses WHERE title = 'Курс 1')
        AND submitted_by = (SELECT id FROM users WHERE email = 'org1@manas.kg')),
-    (SELECT id FROM users WHERE email = 'student12@manas.kg'),
-    'PENDING'
+    (SELECT id FROM users WHERE email = 'student12@manas.kg')
     WHERE NOT EXISTS (
     SELECT 1 FROM course_application_employees
     WHERE employee_id = (SELECT id FROM users WHERE email = 'student12@manas.kg')
 );
 
-INSERT INTO course_application_employees (application_id, employee_id, application_status)
+INSERT INTO course_application_employees (application_id, employee_id)
 SELECT
     (SELECT id FROM course_applications
      WHERE course_id = (SELECT id FROM courses WHERE title = 'Курс 1')
        AND submitted_by = (SELECT id FROM users WHERE email = 'org1@manas.kg')),
-    (SELECT id FROM users WHERE email = 'student13@manas.kg'),
-    'PENDING'
+    (SELECT id FROM users WHERE email = 'student13@manas.kg')
     WHERE NOT EXISTS (
     SELECT 1 FROM course_application_employees
     WHERE employee_id = (SELECT id FROM users WHERE email = 'student13@manas.kg')
