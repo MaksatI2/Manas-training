78464962221b9870919ca52214c2c9cb3c9220eb
Erjan <tasmatoverzan05@gmail.com>
2025-07-04 14:41:47 +0600

    test fix

--- src/test/java/manasTrainingService/ManasTrainingServiceApplicationTests.java
+++ src/test/java/manasTrainingService/ManasTrainingServiceApplicationTests.java
@@ -2,12 +2,13 @@ package manasTrainingService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ManasTrainingServiceApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
+
