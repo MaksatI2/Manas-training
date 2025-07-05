07dd8d5592cf6955b9bef05473e4b4cbf29c2a89
Aislan Erlan uulu <aislanerlan@gmail.com>
2025-07-06 04:23:14 +0600

    Внесение правок по итогам проделанных работ

---
+++ src/main/java/manasTrainingService/controller/rest/CourseApiController.java
@@ -0,0 +1,24 @@
+package manasTrainingService.controller.rest;
+
+import lombok.RequiredArgsConstructor;
+import manasTrainingService.dto.TeacherCardDto;
+import manasTrainingService.service.course.CourseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@RequiredArgsConstructor
+public class CourseApiController {
+
+    private final CourseService courseService;
+
+    @GetMapping("/{id}/teachers")
+    public List<TeacherCardDto> getTeachersByCourse(@PathVariable Integer id) {
+        return courseService.getTeachersByCourse(id);
+    }
+}
