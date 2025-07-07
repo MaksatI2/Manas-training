96931f6dc6e3fd6bb952b32d2bc706fa768a342b
Seitek <seita20078@gmail.com>
2025-07-07 17:08:58 +0600

    фикс миграций заявок

---
+++ src/main/resources/db/changelog/fixes/057-fix-course-employee.sql
@@ -0,0 +1,7 @@
+
+ALTER TABLE course_application_employees
+    ALTER COLUMN application_status SET DEFAULT 'PENDING';
+
+UPDATE course_application_employees
+SET application_status = 'PENDING'
+WHERE application_status = 'pending';
