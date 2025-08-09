20e1de92f41f1b97121a501f0096d3a6868bdad9
Seitek <seita20078@gmail.com>
2025-08-09 23:26:29 +0600

    profile-view, profile-edit

--- src/main/java/manasTrainingService/controller/OrganizationController.java
+++ src/main/java/manasTrainingService/controller/OrganizationController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -35,9 +36,8 @@ public class OrganizationController {
     }
 
     @PostMapping("profile/edit")
-    public String organizationProfileEdit(@Valid OrganizationProfileEditDto organizationProfileEditDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+    public String organizationProfileEdit(@Valid @ModelAttribute("organizationProfile") OrganizationProfileEditDto organizationProfileEditDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("organizationProfile", organizationProfileEditDto);
             return "organization/profile-edit";
         }
         try{
--- src/main/java/manasTrainingService/dto/edit/OrganizationProfileEditDto.java
+++ src/main/java/manasTrainingService/dto/edit/OrganizationProfileEditDto.java
@@ -3,6 +3,7 @@ package manasTrainingService.dto.edit;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import manasTrainingService.validation.ValidPhoneNumber;
 
 @Getter
 @Setter
@@ -13,13 +14,15 @@ public class OrganizationProfileEditDto {
     Integer userId;
 
     @NotBlank(message = "Имя обязательно для заполнения")
+    @Pattern(regexp = "^[a-zA-Zа-яА-ЯёЁ\\s]+$", message = "Поле может содержать только буквы и пробелы")
     String name;
 
     @NotBlank(message = "Фамилия обязательна для заполнения")
+    @Pattern(regexp = "^[a-zA-Zа-яА-ЯёЁ\\s]+$", message = "Поле может содержать только буквы и пробелы")
     String surname;
 
     @NotBlank(message = "Номер телефона не может быть пустым")
-    @Pattern(regexp = "^\\+996\\d{9}$", message = "Номер телефона должен быть в формате +996XXXXXXXXX (12 цифр)")
+    @ValidPhoneNumber
     String phone;
 
     @NotBlank(message = "Названия организации обязательно для заполнения")
