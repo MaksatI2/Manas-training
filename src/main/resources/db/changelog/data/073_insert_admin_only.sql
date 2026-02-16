b41dbb6556264a4936025aa723de0ae13365f051
MaksatI2 <zer0icemax@gmail.com>
2026-02-16 23:07:21 +0600

    fix_40

--- src/main/resources/db/changelog/data/073_insert_admin_only.sql
+++ src/main/resources/db/changelog/data/073_insert_admin_only.sql
@@ -40,3 +40,11 @@ VALUES (
     true
 )
 ON CONFLICT (email) DO NOTHING;
+
+INSERT INTO teacher_profiles (user_id, department, qualifications, bio)
+VALUES (
+    (SELECT id FROM users WHERE email = 'teacher@1manas.kg'),
+    'Aviation',
+    'Master',
+    'Senior instructor'
+);
